11801cd727a1982790354d1bfa317dc62c8771c9
KJoissaint <joissaintkarl@gmail.com>
2026-07-01 00:40:32 +0200

    Guessing game

---
+++ projects/guessing_game/target/debug/deps/cfg_if-50da2642d7d10359.d
@@ -0,0 +1,7 @@
+C:\Users\joiss\OneDrive\Bureau\git projects\learnrust\projects\guessing_game\target\debug\deps\cfg_if-50da2642d7d10359.d: C:\Users\joiss\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\joiss\OneDrive\Bureau\git projects\learnrust\projects\guessing_game\target\debug\deps\libcfg_if-50da2642d7d10359.rlib: C:\Users\joiss\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\joiss\OneDrive\Bureau\git projects\learnrust\projects\guessing_game\target\debug\deps\libcfg_if-50da2642d7d10359.rmeta: C:\Users\joiss\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs
+
+C:\Users\joiss\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.4\src\lib.rs:
